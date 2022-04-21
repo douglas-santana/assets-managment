ab78326fb7968dbe8fb42dcd3ee3ed0e390cc098
Douglas-Santana <douguinhodns@gmail.com>
2022-04-21 19:04:10 -0300

    :bug: fixing update assets

--- src/repositories/implementations/MongoUpdateAssetRepository.ts
+++ src/repositories/implementations/MongoUpdateAssetRepository.ts
@@ -3,9 +3,9 @@ import { IAssetDTO } from "../../DTO/assetDTO";
 import Asset from "../../entities/Asset";
 
 export class MongoUpdateAssetRepository implements IUpdateAssetRepository {
-  async updateAsset(id: string, data: IAssetDTO): Promise<IAssetDTO[]> {
+  async updateAsset(id: string, data: IAssetDTO): Promise<IAssetDTO> {
     const assetUpdated = await Asset.findByIdAndUpdate(
-      id, { $set: { data } },
+      id, { $set: data },
     )
     return assetUpdated;
   }
