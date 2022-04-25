de445aa9c16f8d75fa27e8843d1c06def38712dc
Douglas-Santana <douguinhodns@gmail.com>
2022-04-24 22:43:30 -0300

    :wrench: changed url to connect mongodb atlas

--- src/models/mongoConnection.ts
+++ src/models/mongoConnection.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const mongoConnection =
-mongoose.connect('mongodb+srv://testeTS:testets123@cluster0.5lucr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://testeTS:testeTS@cluster0.5lucr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 
     .then((data) => {
         console.log('MongoDB Connection Succeeded', data.version);
